c54fdc3aff391d98e6f0812a7cd9f530e097cc64
yui <yuineor@gmail.com>
2024-12-14 20:54:27 +0700

    create sql in TaskDao

--- app/src/main/java/com/bignerdranch/android/todo/database/Task.kt
+++ app/src/main/java/com/bignerdranch/android/todo/database/Task.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
+
 @Parcelize
 @Entity(tableName = "task_table")
 data class Task(
