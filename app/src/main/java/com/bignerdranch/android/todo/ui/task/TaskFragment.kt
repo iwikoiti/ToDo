87f5ee9918e6c26e73a7baffafa1aa9ec4c3f98b
yui <yuineor@gmail.com>
2024-12-14 21:15:37 +0700

    add fragments and edit fragment_add.xml, create vector sign plus

---
+++ app/src/main/java/com/bignerdranch/android/todo/ui/task/TaskFragment.kt
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.todo.ui.task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bignerdranch.android.todo.R
+
+class TaskFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task, container, false)
+    }
+}
